f545d8ac4b086738be9f9c379b6c3ff7ac0f5c08
Jonathan <jonathan.hofslundsengen@gmail.com>
2022-05-22 00:35:42 +0200

    done and done

--- main.cpp
+++ main.cpp
@@ -1,34 +1,9 @@
 #include <vector>
-#include <memory>
-#include <cmath>
+#include <cstdint>
+#include <algorithm>
 
 #include <iostream>
 #include <chrono>
-
-constexpr void Traverse(const int X,
-              const int Y,
-              const int ParentIndex,
-              int* CheckIndex,
-              std::vector<int> &CheckList,
-              std::vector<int> &Paths,
-              const std::vector<int> &Map,
-              const std::pair<int, int> &MapDimensions
-) {
-    if ((X < 0 || X >= MapDimensions.first) || (Y < 0 || Y >= MapDimensions.second)) {
-        return;
-    }
-
-    auto index = X + Y * MapDimensions.first;
-    if (Map[index] == 0 || Paths[index] >= 0) {
-        return;
-    }
-    Paths[index] = ParentIndex;
-
-    // Point or index is traversable, save it for later checking
-    // Prioritize points to check
-    CheckList[*CheckIndex] = index;
-    *CheckIndex = *CheckIndex + 1;
-}
 
 bool FindPath(std::pair<int, int> Start,
               std::pair<int, int> Target,
@@ -36,15 +11,11 @@ bool FindPath(std::pair<int, int> Start,
               std::pair<int, int> MapDimensions,
               std::vector<int> &OutPath
 ) {
-    auto epoch2 = std::chrono::high_resolution_clock::from_time_t(0);
-    auto before2   = std::chrono::high_resolution_clock::now();
-    auto before_mseconds2 = duration_cast<std::chrono::nanoseconds>(before2 - epoch2).count();
-
-
-    // List containing which path point to check next. Most back element is the least heavy choice.
+    // List containing which path point to check next. The first elements are the closest to Start which ensures shortest path
     std::vector<int> checkList(Map.size(), -1);
     int checkIndex = 0;
     int checkPlacementIndex = 0;
+
     // History for traversing back to where you started
     std::vector<int> paths(checkList);
 
@@ -55,25 +26,32 @@ bool FindPath(std::pair<int, int> Start,
     paths[startIndex] = startIndex;
     int pathIndex = startIndex;
 
-    auto now2   = std::chrono::high_resolution_clock::now();
-    auto after_mseconds2 = duration_cast<std::chrono::nanoseconds>(now2 - epoch2).count();
-    std::cout << "Time setup: " << (after_mseconds2 - before_mseconds2) << " ns" << std::endl;
-//
-//    auto epoch1 = std::chrono::high_resolution_clock::from_time_t(0);
-//    auto before1   = std::chrono::high_resolution_clock::now();
-//    auto before_mseconds1 = duration_cast<std::chrono::nanoseconds>(before1 - epoch1).count();
-
+    int_fast8_t horizontalCheck[4] = {1, -1, 0, 0};
+    int_fast8_t verticalCheck[4] = {0, 0, 1, -1};
 
     while (pathIndex != targetIndex) {
 
-        // Check this points neighbors and order their weight afterwards.
-        Traverse(x + 1, y, pathIndex, &checkPlacementIndex, checkList, paths, Map, MapDimensions);
-        Traverse(x - 1, y, pathIndex, &checkPlacementIndex, checkList, paths, Map, MapDimensions);
-        Traverse(x, y + 1, pathIndex, &checkPlacementIndex, checkList, paths, Map, MapDimensions);
-        Traverse(x, y - 1, pathIndex, &checkPlacementIndex, checkList, paths, Map, MapDimensions);
+        // Check this points neighbors
+        for (int_fast8_t i = 0; i < 4; i++) {
+            unsigned int xNeighbor = x + horizontalCheck[i];
+            unsigned int yNeighbor = y + verticalCheck[i];
+            if (xNeighbor >= MapDimensions.first || yNeighbor >= MapDimensions.second) {
+                continue;
+            }
+
+            auto index = xNeighbor + yNeighbor * MapDimensions.first;
+            if (Map[index] == 0 || paths[index] >= 0) {
+                continue;
+            }
+            paths[index] = pathIndex;
+
+            // Point or index is traversable, save it for later checking
+            checkList[checkPlacementIndex] = static_cast<int>(index);
+            checkPlacementIndex++;
+        }
 
         // Ready next point to check
-        if (checkIndex < static_cast<int>(checkList.size())) {
+        if (checkIndex < checkPlacementIndex) {
             pathIndex = checkList[checkIndex];
             checkIndex++;
 
@@ -85,22 +63,11 @@ bool FindPath(std::pair<int, int> Start,
         }
     }
 
-//    auto now1   = std::chrono::high_resolution_clock::now();
-//    auto after_mseconds1 = duration_cast<std::chrono::nanoseconds>(now1 - epoch1).count();
-//    std::cout << "Time run: " << (after_mseconds1 - before_mseconds1) << " ns" << std::endl;
-//
-    auto epoch = std::chrono::high_resolution_clock::from_time_t(0);
-    auto before   = std::chrono::high_resolution_clock::now();
-    auto before_mseconds = duration_cast<std::chrono::nanoseconds>(before - epoch).count();
-
     while (pathIndex != startIndex) {
-        OutPath.insert(OutPath.begin(), pathIndex);
+        OutPath.emplace_back(pathIndex);
         pathIndex = paths[pathIndex];
     }
-
-    auto now   = std::chrono::high_resolution_clock::now();
-    auto after_mseconds = duration_cast<std::chrono::nanoseconds>(now - epoch).count();
-    std::cout << "Time after: " << (after_mseconds - before_mseconds) << " ns" << std::endl;
+    std::reverse(OutPath.begin(), OutPath.end());
 
     return true;
 }
@@ -121,19 +88,37 @@ int main() {
 //            0, 1, 0, 1, 1, 1, 1, 1, 0, 1,
 //            0, 1, 1, 0, 1, 0, 0, 0, 1, 1,
 //    };
-//    std::vector<int> OutPath;
-
-
 
     int64_t dimension = 5000;
     std::vector<int> Map(dimension*dimension, 1);
-    std::vector<int> OutPath;
+
+
+//    int64_t dimension = 5000;
+//    std::vector<int> Map(dimension*dimension, 1);
+//    int n = 0;
+//    int y = 1;
+//    int x = 0;
+//    for (auto i = dimension + x; i < dimension*dimension; i++) {
+//        if (n < dimension-1) {
+//            n++;
+//        } else {
+//            y += 2;
+//            if (x == 0)
+//                x = 1;
+//            else
+//                x = 0;
+//            i = dimension*y + x;
+//            n = 1;
+//        }
+//        Map[i] = 0;
+//    }
 
 //    std::vector<int> Map = {1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1};
-//    std::vector<int> OutPath;
 
 //    std::vector<int> Map = {0, 0, 1, 0, 1, 1, 1, 0, 1};
-//    std::vector<int> OutPath;
+
+
+    std::vector<int> OutPath;
 
     auto epoch = std::chrono::high_resolution_clock::from_time_t(0);
     auto before   = std::chrono::high_resolution_clock::now();
@@ -142,6 +127,7 @@ int main() {
     std::cout << "Starting..." << std::endl;
 //        auto foundPath = FindPath({0, 0}, {9, 10}, Map, {10, 11}, OutPath);
     auto foundPath = FindPath({0, 0}, {dimension-1, dimension-1}, Map, {dimension, dimension}, OutPath);
+//    auto foundPath = FindPath({0, 0}, { n < y ? dimension - 1 : 0, dimension-1}, Map, {dimension, dimension}, OutPath);
 //    auto foundPath = FindPath({0, 0}, {1, 2}, Map, {4, 3}, OutPath);
 //    auto foundPath = FindPath({2, 0}, {0, 2}, Map, {3, 3}, OutPath);
 
